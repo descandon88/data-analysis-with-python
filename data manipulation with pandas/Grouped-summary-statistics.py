9dfe8416d2c529510f4829c974a9ae92c7ae1f7a
David Escandón <david.escandon.v@gmail.com>
2022-05-17 13:09:51 -0300

    Update Grouped-summary-statistics.py

--- data manipulation with pandas/Grouped-summary-statistics.py
+++ data manipulation with pandas/Grouped-summary-statistics.py
@@ -25,3 +25,19 @@ sales_by_type = sales.groupby("type")["weekly_sales"].sum()
 # Group by type and is_holiday; calc total weekly sales
 sales_by_type_is_holiday = sales.groupby(["type","is_holiday"])["weekly_sales"].sum()
 print(sales_by_type_is_holiday)
+
+
+# Import numpy with the alias np
+import numpy as np
+
+# For each store type, aggregate weekly_sales: get min, max, mean, and median
+sales_stats = sales.groupby("type")["weekly_sales"].agg([max,min,np.mean,np.median])
+
+# Print sales_stats
+print(sales_stats)
+
+# For each store type, aggregate unemployment and fuel_price_usd_per_l: get min, max, mean, and median
+unemp_fuel_stats = sales.groupby("type")[["unemployment","fuel_price_usd_per_l"]].agg([max,min,np.mean,np.median])
+
+# Print unemp_fuel_stats
+print(unemp_fuel_stats)
