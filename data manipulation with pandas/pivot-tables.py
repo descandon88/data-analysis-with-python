69ddfa535cc4d403677a07a45922e6fd4f31e905
David Escandón <david.escandon.v@gmail.com>
2022-05-17 14:06:30 -0300

    Update pivot-tables.py

--- data manipulation with pandas/pivot-tables.py
+++ data manipulation with pandas/pivot-tables.py
@@ -1,1 +1,21 @@
+import numpy as np
+
+
+# Pivot for mean and median weekly_sales for each store type
+mean_med_sales_by_type = sales.pivot_table(values="weekly_sales", index="type", aggfunc=[np.mean, np.median])
+
+# Print mean_med_sales_by_type
+print(mean_med_sales_by_type)
+
+# Pivot for mean weekly_sales by store type and holiday 
+#mean_sales_by_type_holiday = sales.pivot_table(values="weekly_sales",index="type", columns="is_holiday", aggfunc=[np.mean])
+
+mean_sales_by_type_holiday = sales.pivot_table(values="weekly_sales",index="type", columns="is_holiday", margins=False)
+
+# Print mean_sales_by_type_holiday
+print(mean_sales_by_type_holiday)
+
+# Print the mean weekly_sales by department and type; fill missing values with 0s; sum all rows and cols
+print(sales.pivot_table(values="weekly_sales", index="department", columns="type", fill_value=0,margins=True))
+
 
