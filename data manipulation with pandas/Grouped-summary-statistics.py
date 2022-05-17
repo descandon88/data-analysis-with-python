b2452bd02601c7b3bcf5c421c65297500cd8784c
David Escandón <david.escandon.v@gmail.com>
2022-05-17 12:53:45 -0300

    Update Grouped-summary-statistics.py

--- data manipulation with pandas/Grouped-summary-statistics.py
+++ data manipulation with pandas/Grouped-summary-statistics.py
@@ -18,3 +18,10 @@ print(sales_propn_by_type)
 
 
 # Calculations with .groupby()
+# The .groupby() method makes life much easier. In this exercise, you'll perform the same calculations as last time, except you'll use the .groupby() method. You'll also perform calculations on data grouped by two variables to see if sales differ by store type depending on if it's a holiday week or not.
+# From previous step
+sales_by_type = sales.groupby("type")["weekly_sales"].sum()
+
+# Group by type and is_holiday; calc total weekly sales
+sales_by_type_is_holiday = sales.groupby(["type","is_holiday"])["weekly_sales"].sum()
+print(sales_by_type_is_holiday)
