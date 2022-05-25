67f6520308e887df72e68f325209569619c88363
David Escandón <david.escandon.v@gmail.com>
2022-05-25 14:36:49 -0300

    Update summary-statistics.py

--- statistical-thinking-python/summary-statistics.py
+++ statistical-thinking-python/summary-statistics.py
@@ -1,2 +1,9 @@
 #Computing means
 #The mean of all measurements gives an indication of the typical magnitude of a measurement. It is computed using np.mean().
+
+# Compute the mean: mean_length_vers
+import numpy as np
+mean_length_vers=np.mean(versicolor_petal_length)
+
+# Print the result with some nice formatting
+print('I. versicolor:', mean_length_vers, 'cm')
