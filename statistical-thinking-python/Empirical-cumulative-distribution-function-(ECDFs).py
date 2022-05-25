29276d93156c8960f12ca0a5902e0d45d0e186eb
David Escandón <david.escandon.v@gmail.com>
2022-05-24 22:43:14 -0300

    Create Empirical-cumulative-distribution-function-(ECDFs).py

---
+++ statistical-thinking-python/Empirical-cumulative-distribution-function-(ECDFs).py
@@ -0,0 +1,16 @@
+import numpy as np
+x = np.sort()
+y = np.arange()
+
+def ecdf(data):
+    """Compute ECDF for a one-dimensional array of measurements."""
+    # Number of data points: n
+    n = len(data)
+
+    # x-data for the ECDF: x
+    x = np.sort(data)
+
+    # y-data for the ECDF: y
+    y = np.arange(1, len(x)+1) / n
+
+    return x, y
