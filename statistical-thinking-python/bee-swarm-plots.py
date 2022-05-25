fc2f2c628b8d49877fe05d05d6d6eb896f9d7ff6
David Escandón <david.escandon.v@gmail.com>
2022-05-25 14:20:57 -0300

    Update bee-swarm-plots.py

--- statistical-thinking-python/bee-swarm-plots.py
+++ statistical-thinking-python/bee-swarm-plots.py
@@ -7,4 +7,18 @@ _ = plt.ylabel('petal length (cm)')
 # Show the plot
 plt.show()
 
+#Plotting the ECDF
+#You will now use your ecdf() function to compute the ECDF for the petal lengths of Anderson's Iris versicolor flowers. You will then plot the ECDF. Recall that your ecdf() function returns two arrays so you will need to unpack them. An example of such unpacking is x, y = foo(data), for some function foo()
+# Compute ECDF for versicolor data: x_vers, y_vers
+x_vers, y_vers = ecdf(versicolor_petal_length)
 
+# Generate plot
+plt.plot(x_vers,y_vers,marker=".",linestyle = 'none')
+
+# Label the axes
+_ = plt.xlabel('x_vers')
+_ = plt.ylabel('y_vers')
+
+
+# Display the plot
+plt.show()
