7df2f531e9b110bef9cf2e71f3248db19f183f20
David Escandón <david.escandon.v@gmail.com>
2022-05-26 19:15:35 -0300

    Update variance-standard_deviation.py

--- statistical-thinking-python/variance-standard_deviation.py
+++ statistical-thinking-python/variance-standard_deviation.py
@@ -33,7 +33,38 @@ _ = plt.plot(versicolor_petal_length,versicolor_petal_width, marker='.', linesty
 _ = plt.xlabel('versicolor_petal_length')
 _ = plt.ylabel('versicolor_petal_width')
 
-
-
 # Show the result
 plt.show()
+
+# Computing the covariance
+# Compute the covariance matrix: covariance_matrix
+covariance_matrix=np.cov(versicolor_petal_length, versicolor_petal_width)
+
+# Print covariance matrix
+print(covariance_matrix)
+
+# Extract covariance of length and width of petals: petal_cov
+petal_cov=covariance_matrix[0 ,1]
+
+# Print the length/width covariance
+print(petal_cov)
+
+# Computing the Pearson correlation coefficient
+# In this exercise, you will write a function, pearson_r(x, y) that takes in two arrays and returns the Pearson correlation coefficient.
+#You will then use this function to compute it for the petal lengths and widths of I. versicolor. Again, we include the scatter plot you
+#generated in a previous exercise to remind you how the petal width and length are related.
+
+def pearson_r(x, y):
+    """Compute Pearson correlation coefficient between two arrays."""
+    # Compute correlation matrix: corr_mat
+    corr_mat=np.corrcoef(x,y)
+
+    # Return entry [0,1]
+    return corr_mat[0,1]
+
+# Compute Pearson correlation coefficient for I. versicolor: r
+r=pearson_r(versicolor_petal_length,versicolor_petal_width)
+
+# Print the result
+print(r)
+
