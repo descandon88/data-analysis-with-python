50c1b0245bd1c327f792553494f294e145d40661
David Escandón <david.escandon.v@gmail.com>
2022-05-26 18:48:51 -0300

    Update variance-standard_deviation.py

--- statistical-thinking-python/variance-standard_deviation.py
+++ statistical-thinking-python/variance-standard_deviation.py
@@ -23,3 +23,17 @@ print(np.sqrt(variance))
 
 # Print the standard deviation
 print(np.std(versicolor_petal_length))
+
+# Scatter plots
+# Make a scatter plot
+_ = plt.plot(versicolor_petal_length,versicolor_petal_width, marker='.', linestyle='none')
+
+
+# Label the axes
+_ = plt.xlabel('versicolor_petal_length')
+_ = plt.ylabel('versicolor_petal_width')
+
+
+
+# Show the result
+plt.show()
