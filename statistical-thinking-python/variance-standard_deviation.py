2428e72c461cbac6dffa803d98518b6d281a0d8f
David Escandón <david.escandon.v@gmail.com>
2022-05-26 17:00:21 -0300

    Update and rename variance-standard-deviation.py to variance-standard_deviation.py

---
+++ statistical-thinking-python/variance-standard_deviation.py
@@ -0,0 +1,25 @@
+# Computing the variance
+# Array of differences to mean: differences
+differences=versicolor_petal_length-np.mean(versicolor_petal_length)
+
+# Square the differences: diff_sq
+diff_sq=differences**2
+
+# Compute the mean square difference: variance_explicit
+variance_explicit=np.mean(diff_sq)
+
+# Compute the variance using NumPy: variance_np
+variance_np=np.var(versicolor_petal_length)
+
+# Print the results
+print(variance_explicit,variance_np)
+
+# The standard deviation and the variance
+# Compute the variance: variance
+variance=np.var(versicolor_petal_length)
+
+# Print the square root of the variance
+print(np.sqrt(variance))
+
+# Print the standard deviation
+print(np.std(versicolor_petal_length))
