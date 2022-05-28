aded122f0b1993dc0b71306202d9429e1393c642
David Escandón <david.escandon.v@gmail.com>
2022-05-28 17:15:30 -0300

    Update the-binomial-distribution.py

--- statistical-thinking-python/the-binomial-distribution.py
+++ statistical-thinking-python/the-binomial-distribution.py
@@ -1,1 +1,14 @@
+# Plotting the Binomial PMF
 
+# Compute bin edges: bins
+bins = np.arange(0, max(n_defaults) + 1.5) - 0.5
+
+# Generate histogram
+plt.hist(n_defaults,normed=True,bins=bins)
+
+# Label axes
+_ = plt.xlabel('')
+_ = plt.ylabel('')
+
+# Show the plot
+plt.show()
