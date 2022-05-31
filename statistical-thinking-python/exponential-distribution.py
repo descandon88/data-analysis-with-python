377db152a77413b050ab691b059c8a794f43c65e
David Escandón <david.escandon.v@gmail.com>
2022-05-31 14:36:46 -0300

    Update exponential-distribution.py

--- statistical-thinking-python/exponential-distribution.py
+++ statistical-thinking-python/exponential-distribution.py
@@ -9,3 +9,18 @@ def successive_poisson(tau1, tau2, size=1):
     t2 = np.random.exponential(tau2, size)
 
     return t1 + t2
+
+# Draw samples of waiting times: waiting_times
+waiting_times=successive_poisson(764,715,100000)
+
+# Make the histogram
+plt.hist(waiting_times,bins=100,normed=True,histtype='step')
+
+
+# Label axes
+_ = plt.xlabel('time (days)')
+_ = plt.ylabel('CDF')
+
+
+# Show the plot
+plt.show()
