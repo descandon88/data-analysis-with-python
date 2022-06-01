9cb5c3333c25e132a3d95e8e50c2d9a8c2b7800f
David Escandón <david.escandon.v@gmail.com>
2022-06-01 19:46:21 -0300

    Update Explicit-indexes.py

--- data manipulation with pandas/Explicit-indexes.py
+++ data manipulation with pandas/Explicit-indexes.py
@@ -31,3 +31,14 @@ rows_to_keep = [('Brazil','Rio De Janeiro'),('Pakistan','Lahore')]
 
 # Subset for rows to keep
 print(temperatures_ind.loc[rows_to_keep])
+
+#Sorting by index values
+
+# Sort temperatures_ind by index values
+print(temperatures_ind.sort_index())
+
+# Sort temperatures_ind by index values at the city level
+print(temperatures_ind.sort_index(level='city'))
+
+# Sort temperatures_ind by country then descending city
+print(temperatures_ind.sort_index(level=['country','city'],ascending=[True,False]))
