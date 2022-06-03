db4f3cb5df2cee1076fcc80fda3ad39265540745
David Escandón <david.escandon.v@gmail.com>
2022-06-03 19:16:04 -0300

    Update slicing-and-subsetting-with-.loc-and-.iloc.py

--- data manipulation with pandas/slicing-and-subsetting-with-.loc-and-.iloc.py
+++ data manipulation with pandas/slicing-and-subsetting-with-.loc-and-.iloc.py
@@ -11,3 +11,13 @@ print(temperatures_srt.loc['Lahore':'Moscow'])
 print(temperatures_srt.loc[('Pakistan','Lahore'):('Russia','Moscow')])
 
 # Slicing in both directions
+# Subset rows from India, Hyderabad to Iraq, Baghdad
+print(temperatures_srt.loc[('India','Hyderabad'):('Iraq','Baghdad')])
+
+# Subset columns from date to avg_temp_c
+print(temperatures_srt.loc[:,'date':'avg_temp_c'])
+
+# Subset in both directions at once
+print(temperatures_srt.loc[('India','Hyderabad'):('Iraq','Baghdad'),'date':'avg_temp_c'])
+
+# Slicing time series
