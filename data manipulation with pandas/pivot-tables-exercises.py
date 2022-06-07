4cc31eaf5bd9035bf95eaf04be8e06a96b60bda3
David Escandón <david.escandon.v@gmail.com>
2022-06-07 18:31:51 -0300

    Update and rename pivot-tables-exercise.py to pivot-tables-exercises.py

--- data manipulation with pandas/pivot-tables-exercises.py
+++ data manipulation with pandas/pivot-tables-exercises.py
@@ -20,3 +20,16 @@ temp_by_country_city_vs_year.loc[('Egypt','Cairo'):('India','Delhi')]
 temp_by_country_city_vs_year.loc[('Egypt','Cairo'):('India','Delhi'),'2005':'2010']
 
 print(temp_by_country_city_vs_year)
+
+# Calculating on a pivot table
+# Get the worldwide mean temp by year
+mean_temp_by_year = temp_by_country_city_vs_year.mean()
+
+# Filter for the year that had the highest mean temp
+print(mean_temp_by_year[mean_temp_by_year==mean_temp_by_year.max()])
+
+# Get the mean temp by city
+mean_temp_by_city = temp_by_country_city_vs_year.mean(axis='columns')
+
+# Filter for the city that had the lowest mean temp
+print(mean_temp_by_city[mean_temp_by_city==mean_temp_by_city.min()])
