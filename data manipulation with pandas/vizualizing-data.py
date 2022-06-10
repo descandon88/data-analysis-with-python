136f81f2360e878afd279ce00dfccf33b1b73336
David Escandón <david.escandon.v@gmail.com>
2022-06-10 19:54:37 -0300

    Update vizualizing-data.py

--- data manipulation with pandas/vizualizing-data.py
+++ data manipulation with pandas/vizualizing-data.py
@@ -14,3 +14,18 @@ nb_sold_by_size.plot(kind='bar')
 
 # Show the plot
 plt.show()
+
+
+# Changes in sales over time
+# Import matplotlib.pyplot with alias plt
+import matplotlib.pyplot as plt
+
+# Get the total number of avocados sold on each date
+nb_sold_by_date = avocados.groupby('date')['nb_sold'].sum()
+
+# Create a line plot of the number of avocados sold by date
+nb_sold_by_date.plot()
+
+# Show the plot
+plt.show()
+
