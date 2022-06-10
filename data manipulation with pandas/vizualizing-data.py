3c330af2222dac9c22f158c317dc3702e0fcf0dc
David Escandón <david.escandon.v@gmail.com>
2022-06-10 20:13:39 -0300

    Update vizualizing-data.py

--- data manipulation with pandas/vizualizing-data.py
+++ data manipulation with pandas/vizualizing-data.py
@@ -29,3 +29,11 @@ nb_sold_by_date.plot()
 # Show the plot
 plt.show()
 
+# Scatter plot of avg_price vs. nb_sold with title
+avocados.plot(x='nb_sold',y='avg_price', kind='scatter',title='Number of avocados sold vs. average price')
+
+# Show the plot
+plt.xlabel("nb_sold")
+plt.ylabel("avg_price")
+
+plt.show()
