052dedb4a5b2c8dd3450aeec155aa9a77f984965
David Escandón <david.escandon.v@gmail.com>
2022-06-11 14:13:21 -0300

    Update missing-values.py

--- data manipulation with pandas/missing-values.py
+++ data manipulation with pandas/missing-values.py
@@ -2,11 +2,13 @@
 # Import matplotlib.pyplot with alias plt
 import matplotlib.pyplot as plt
 
+avocados_2016.head(10)
+
 # Check individual values for missing values
-print(avocados_2016.isna())
+print(avocados_2016.isna().any())
 
 # Check each column for missing values
-print(avocados_2016.isna().sum())
+print(avocados_2016.isna().any().sum())
 
 # Bar plot of missing values by variable
 avocados_2016.isna().sum().plot(kind='bar')
