5edb6fdba2ed4421bf4da3793574c2d92d75346f
David Escandón <david.escandon.v@gmail.com>
2022-06-11 13:11:28 -0300

    Update vizualizing-data.py

--- data manipulation with pandas/vizualizing-data.py
+++ data manipulation with pandas/vizualizing-data.py
@@ -37,3 +37,16 @@ plt.xlabel("nb_sold")
 plt.ylabel("avg_price")
 
 plt.show()
+
+#Price of conventional vs. organic avocados
+# Histogram of conventional avg_price 
+avocados[avocados['type']=='conventional']['avg_price'].hist()
+
+# Histogram of organic avg_price
+avocados[avocados['type']=='organic']['avg_price'].hist()
+
+# Add a legend
+plt.legend(['conventional','organic'])
+
+# Show the plot
+plt.show()
