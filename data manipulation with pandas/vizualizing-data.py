19db2ff96ef60e7a0423bbd1458626df6cde7a99
David Escandón <david.escandon.v@gmail.com>
2022-06-11 13:15:45 -0300

    Update vizualizing-data.py

--- data manipulation with pandas/vizualizing-data.py
+++ data manipulation with pandas/vizualizing-data.py
@@ -50,3 +50,31 @@ plt.legend(['conventional','organic'])
 
 # Show the plot
 plt.show()
+
+# Modify your code to adjust the transparency of both histograms to 0.5 to see how much overlap there is between the two distributions.
+# Modify histogram transparency to 0.5 
+avocados[avocados["type"] == "conventional"]["avg_price"].hist(alpha=0.5)
+
+# Modify histogram transparency to 0.5
+avocados[avocados["type"] == "organic"]["avg_price"].hist(alpha=0.5)
+
+# Add a legend
+plt.legend(["conventional", "organic"])
+
+# Show the plot
+plt.show()
+
+# Modify your code to use 20 bins in both histograms.
+# Modify bins to 20
+avocados[avocados["type"] == "conventional"]["avg_price"].hist(alpha=0.5,bins=20)
+
+# Modify bins to 20
+avocados[avocados["type"] == "organic"]["avg_price"].hist(alpha=0.5,bins=20)
+
+# Add a legend
+plt.legend(["conventional", "organic"])
+
+# Show the plot
+plt.show()
+
+
