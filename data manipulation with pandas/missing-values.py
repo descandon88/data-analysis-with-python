381171c8130362d1bde42bf7905a0397dfe2cc66
David Escandón <david.escandon.v@gmail.com>
2022-06-11 14:35:31 -0300

    Update missing-values.py

--- data manipulation with pandas/missing-values.py
+++ data manipulation with pandas/missing-values.py
@@ -23,3 +23,14 @@ avocados_complete = avocados_2016.dropna()
 
 # Check if any columns contain missing values
 print(avocados_complete.isna().any())
+
+
+# Replacing missing values
+# List the columns with missing values
+cols_with_missing = ["small_sold", "large_sold", "xl_sold"]
+
+# Create histograms showing the distributions cols_with_missing
+avocados_2016[cols_with_missing].hist()
+
+# Show the plot
+plt.show()
