b59dcb71b4de1a41bd78afb9fc1bdd0d59a9534b
David Escandón <david.escandon.v@gmail.com>
2022-06-12 15:39:14 -0300

    Update importing-HDF5-files.py

--- importing-data-from-other-file-types/importing-HDF5-files.py
+++ importing-data-from-other-file-types/importing-HDF5-files.py
@@ -19,3 +19,30 @@ print(type(data))
 # Print the keys of the file
 for key in data.keys():
     print(key)
+    
+#Extracting data from your HDF5 file
+#In this exercise, you'll extract some of the LIGO experiment's actual data from the HDF5 file and you'll visualize it.
+#To do so, you'll need to first explore the HDF5 group 'strain'
+
+# Get the HDF5 group: group
+group=data['strain']
+
+# Check out keys of group
+for key in group.keys():
+    print(key)
+
+# Set variable equal to time series data: strain
+strain=np.array(data['strain']['Strain'])
+
+# Set number of time points to sample: num_samples
+num_samples=10000
+
+# Set time vector
+time = np.arange(0, 1, 1/num_samples)
+
+# Plot data
+plt.plot(time, strain[:num_samples])
+plt.xlabel('GPS Time (s)')
+plt.ylabel('strain')
+plt.show()
+
