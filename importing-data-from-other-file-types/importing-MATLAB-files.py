899001da94193309223cd6d879c925ea45527808
David Escandón <david.escandon.v@gmail.com>
2022-06-12 15:44:29 -0300

    Update importing-MATLAB-files.py

--- importing-data-from-other-file-types/importing-MATLAB-files.py
+++ importing-data-from-other-file-types/importing-MATLAB-files.py
@@ -1,1 +1,16 @@
+#import scipy.io
+#file='file'
+#mat=scipy.io.loadmat(file)
 
+import scipy.io
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Import package
+import scipy.io
+
+# Load MATLAB file: mat
+mat=scipy.io.loadmat('albeck_gene_expression.mat')
+
+# Print the datatype type of mat
+print(type(mat))
