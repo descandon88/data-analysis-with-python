760ae359fd072d5fe4be97be05d25ce90e556488
David Escandón <david.escandon.v@gmail.com>
2022-06-12 15:37:03 -0300

    Update importing-HDF5-files.py

--- importing-data-from-other-file-types/importing-HDF5-files.py
+++ importing-data-from-other-file-types/importing-HDF5-files.py
@@ -1,3 +1,21 @@
 #IMPORTING HDG5 FILES
 # Hierarchical Data Format version5
 # Standard for storing large quantities of numerical data
+# HDF5 can scale exabytes
+
+# Import packages
+import numpy as np
+import h5py
+
+# Assign filename: file
+file='LIGO_data.hdf5'
+
+# Load file: data
+data = h5py.File(file, 'r')
+
+# Print the datatype of the loaded file
+print(type(data))
+
+# Print the keys of the file
+for key in data.keys():
+    print(key)
