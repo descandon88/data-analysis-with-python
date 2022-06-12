1b857c90aa02e9ccc1222cfb72fd79280e5bdc36
David Escandón <david.escandon.v@gmail.com>
2022-06-12 18:18:01 -0300

    Update sql-queries.py

--- importing-data-from-other-file-types/sql-queries.py
+++ importing-data-from-other-file-types/sql-queries.py
@@ -48,3 +48,20 @@ with engine.connect() as con:
 
 # Print the head of the DataFrame df
 print(df.head())
+
+
+
+
+# Create engine: engine
+engine=create_engine('sqlite:///Chinook.sqlite')
+
+# Open engine in context manager
+with engine.connect() as con:
+    rs = con.execute('SELECT * FROM Employee ORDER by BirthDate')
+    df = pd.DataFrame(rs.fetchall())
+
+    # Set the DataFrame's column names
+    df.columns=rs.keys()
+
+# Print head of DataFrame
+print(df.head())
