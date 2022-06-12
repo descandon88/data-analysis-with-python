cf8a37fe078edea195bb57669ce8deb53217ad8c
David Escandón <david.escandon.v@gmail.com>
2022-06-12 19:12:16 -0300

    Update sql-queries.py

--- importing-data-from-other-file-types/sql-queries.py
+++ importing-data-from-other-file-types/sql-queries.py
@@ -65,3 +65,37 @@ with engine.connect() as con:
 
 # Print head of DataFrame
 print(df.head())
+
+
+# Pandas and The Hello World of SQL Queries!
+
+# Print head of DataFrame
+print(df.head())
+
+# Open engine in context manager and store query result in df1
+with engine.connect() as con:
+    rs = con.execute("SELECT * FROM Album")
+    df1 = pd.DataFrame(rs.fetchall())
+    df1.columns = rs.keys()
+
+# Confirm that both methods yield the same result
+print(df.equals(df1))
+
+
+#Pandas for more complex querying
+#Here, you'll become more familiar with the pandas function read_sql_query() by using it to execute a more complex query: a SELECT statement followed by both a WHERE clause AND an ORDER BY clause.
+
+#You'll build a DataFrame that contains the rows of the Employee table for which the EmployeeId is greater than or equal to 6 and you'll order these entries by BirthDate.
+# Import packages
+# Import packages
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+engine=create_engine('sqlite:///Chinook.sqlite')
+
+# Execute query and store records in DataFrame: df
+df=pd.read_sql_query('SELECT * FROM Employee WHERE EmployeeId >= 6   ORDER by  BirthDate',engine)
+
+# Print head of DataFrame
+print(df.head())
