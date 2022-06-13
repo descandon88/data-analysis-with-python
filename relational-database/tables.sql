7f5dc34b55a18575f10f7192eb47213518207c18
David Escandón <david.escandon.v@gmail.com>
2022-06-13 19:58:56 -0300

    Update tables.sql

--- relational-database/tables.sql
+++ relational-database/tables.sql
@@ -17,3 +17,12 @@ CREATE TABLE universities (
 SELECT * 
 FROM universities
 
+-- Alter professors to add the text column university_shortname.
+
+-- Add the university_shortname column
+ALTER TABLE professors 
+ADD COLUMN university_shortname text;
+
+-- Print the contents of this table
+SELECT * 
+FROM professors
