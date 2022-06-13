a96d967abf6380b84c39b2fc859fe3f468c08d75
David Escandón <david.escandon.v@gmail.com>
2022-06-13 19:56:01 -0300

    Update and rename relational-database.sql to relational-database/tables.sql

---
+++ relational-database/tables.sql
@@ -0,0 +1,19 @@
+-- You'll now start implementing a better database model. For this, you'll create tables for the professors and universities entity types. The other tables will be created for you.
+
+-- The syntax for creating simple tables is as follows:
+
+-- Create a table for the universities entity type
+CREATE TABLE universities {
+    university_shortname text,
+    university text,
+    university_city text
+};
+
+
+
+
+
+-- Print the contents of this table
+SELECT * 
+FROM universities
+
